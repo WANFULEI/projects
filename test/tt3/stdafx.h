880604a4803694b7239c8b0133874286775a3dd6
wq <897810981@outlook.com>
2016-06-25 14:46:57 +0800

    111

--- test/tt3/stdafx.h
+++ test/tt3/stdafx.h
@@ -5,7 +5,7 @@ using namespace std;
 
 #include <QtGui/QApplication>
 
-#include "..\..\..\control\layerwidget\Qt_default_widget.h"
+#include "..\..\control\layerwidget\Qt_default_widget.h"
 using namespace layerwidget;
 
 #include "global.h"
